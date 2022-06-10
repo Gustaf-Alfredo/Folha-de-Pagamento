dd6a3b026fd9a7d88f3215558b9ec53a8d95f9c4
Gustavo Alfredo <101288917+Gustaf-Alfredo@users.noreply.github.com>
2022-06-10 15:32:24 -0300

    update

--- app.js
+++ app.js
@@ -16,22 +16,50 @@
     const btCalcular = document.getElementById('btCalcular');
     const btLimpar = document.getElementById('btLimpar');
 
-    //resposta do programa
+    //resposta de validação - outValidacao
+    const outValidacaoNomeHelp = document.getElementById('outValidacaoNomeHelp');
+    const outValidacaoNomeIcon = document.getElementById('outValidacaoNomeIcon');
+    const outValidacaoBorda = document.querySelector(".outValidacaoBorda")
+
+    //resposta do programa - out
     const outResposta = document.getElementById('outResposta');
+
+
 
 //iniciando programa
     function calcular () {
     //puxando valores
         let nome = inNome.value;
         let salBruto = Number(inSalBruto.value);
-        if(nome == "") {
-            //método para mudar o placeholder
-             /* document.getElementsByName('ValidacaoNome').style.background = '2px solid blue'; */
-             inNome.style.border = '2px solid #FF2B56'
-             
-        }
 
+    //validação para nome
+        if(nome == "") {
+            inNome.style.border = '2px solid #FF2B56';
+            outValidacaoNomeHelp.textContent = 'Por favor, preencha corretamente o formulário...' ;           
+            outValidacaoNomeIcon.classList.remove("ValidacaoIcon");
+        } else
+    //validação para salário bruto
+        if (salBruto >= 0) {
+            
+            window.alert('erro')
+           
+        }
+        console.log(salBruto)
     }
-    
+
+    function resetNome() {
+        inNome.style.border = 'none';
+        outValidacaoBorda.style.border = '0.1px solid #dbdbdb';
+        outValidacaoNomeHelp.textContent = ''
+        outValidacaoNomeIcon.classList.add('ValidacaoIcon');
+    } 
+
+
+
+
+    //acionamento de eventos
     btCalcular.addEventListener("click", calcular);
+    inNome.addEventListener("click", resetNome)
+
+
 
