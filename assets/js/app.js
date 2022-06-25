fdac6e75b7af049a49dab9bc78267b791634a624
Gustavo Alfredo <101288917+Gustaf-Alfredo@users.noreply.github.com>
2022-06-25 14:41:16 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@ class App {
         let pensao = document.querySelector("input[name='checkPensao']").checked
         let dependente = document.querySelector("input[name='checkDependente']").checked
         let funcionario = new Funcionario(nome,inSalBruto,dependente,pensao)
-
+        console.log(funcionario)
         this.salBruto = inSalBruto
 
         if(dependente) {
@@ -15,9 +15,41 @@ class App {
         }
         if(pensao) {
             window.alert('check - pensao')
-        }
+        }  
+        
+        //let INSSdescontoVariavel = this.INSSdesconto(1212.01,2427.35,3641.03,7087.22,/**/0.075,0.09,0.12,0.14)
+        let INSSdescontoVariavel = this.INSSdesconto(this.descontoINSS,1212.01,2427.35,3641.03,7087.22,/**/0.075,0.09,0.12,0.14)
+        console.log(INSSdescontoVariavel)
     }
 
+    INSSdesconto(descontoINSS,/**/tetoINSS1,tetoINSS2,tetoINSS3,tetoINSS4,/**/taxaINSS1,taxaINSS2,taxaINSS3,taxaINSS4) {
+        
+        this.descontoINSS = descontoINSS
+        if(this.salBruto <= tetoINSS1) {
+            descontoINSS = this.salBruto * taxaINSS1
+            console.log(this.salBruto)
+        } else 
+        if (this.salBruto > tetoINSS1 && this.salBruto <= tetoINSS2) {
+            descontoINSS = this.salBruto * taxaINSS2
+        } else
+        if(this.salBruto > tetoINSS2 && this.salBruto <= tetoINSS3) {
+            descontoINSS = this.salBruto * taxaINSS3
+        } else 
+        if(this.salBruto > tetoINSS3 && this.salBruto <= tetoINSS4) {
+            descontoINSS = this.salBruto * taxaINSS4
+        } else
+        if(this.salBruto > tetoINSS4) {
+            descontoINSS = tetoINSS4 * taxaINSS4
+        }
+        console.log(descontoINSS)
+        return descontoINSS
+    }
 
+    calcular() {
+        let INSSdescontoVariavel = this.INSSdesconto(1212.01,2427.35,3641.03,7087.22,/**/0.075,0.09,0.12,0.14)
+        console.log(INSSdescontoVariavel)
+        return INSSdescontoVariavel
+        
+    }
 }
 
--- assets/js/funcionario.js
+++ assets/js/funcionario.js
@@ -4,34 +4,6 @@ class Funcionario {
         this.salBrutoClass = salBruto
         this.dependenteCLass = dependente
         this.pensaoClass = pensao
-        this.salLiquidoClass = salLiquido
-        
-    }
-
-    static INSSdesconto(descontoINSS,/**/tetoINSS1,tetoINSS2,tetoINSS3,tetoINSS4,/**/taxaINSS1,taxaINSS2,taxaINSS3,taxaINSS4) {
-        this.descontoINSS = descontoINSS
-        
-        if(this.salBruto <= tetoINSS1) {
-            descontoINSS = this.salBruto * taxaINSS1
-        } else 
-        if (this.salBruto > tetoINSS1 && this.salBruto <= tetoINSS2) {
-            descontoINSS = this.salBruto * taxaINSS2
-        } else
-        if(this.salBruto > tetoINSS2 && this.salBruto <= tetoINSS3) {
-            descontoINSS = this.salBruto * taxaINSS3
-        } else 
-        if(this.salBruto > tetoINSS3 && this.salBruto <= tetoINSS4) {
-            descontoINSS = this.salBruto * taxaINSS4
-        } else
-        if(this.salBruto > tetoINSS4) {
-            descontoINSS = tetoINSS4 * taxaINSS4
-        }
-        console.log(descontoINSS)
-        return descontoINSS
-    }
-
-    static get calcular() {
-        let INSSdescontoVariavel = this.INSSdesconto(1212.01,2427.35,3641.03,7087.22,/**/0.075,0.09,0.12,0.14)
-        return INSSdescontoVariavel
+        this.salLiquidoClass = salLiquido      
     }
 }
