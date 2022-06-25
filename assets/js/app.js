f09c0194f95e27f44cc26d06f7b0bcd13789eb39
Gustavo Alfredo <gustavoalfredo.web@gmail.com>
2022-06-25 01:19:16 -0300

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,23 @@
-class app {
-    
+class App {
+    //método para registrar funcionários
+    RegistrarFuncionarios() {
+        //referências
+        let nome = document.querySelector("input[name='inNome']").value
+        let inSalBruto = document.querySelector("input[name='inSalBruto']").value
+        let pensao = document.querySelector("input[name='checkPensao']").checked
+        let dependente = document.querySelector("input[name='checkDependente']").checked
+        let funcionario = new Funcionario(nome,inSalBruto,dependente,pensao)
+
+        this.salBruto = inSalBruto
+
+        if(dependente) {
+            window.alert('check - dependente')
+        }
+        if(pensao) {
+            window.alert('check - pensao')
+        }
+    }
+
+
 }
+
--- assets/js/funcionario.js
+++ assets/js/funcionario.js
@@ -1,9 +1,36 @@
-class funcionario {
-    constructor (nome,salBruto,pensao,dependente,salLiquido) {
-        this.nome = nome
-        this.salBruto = salBruto
-        this.pensao = pensao
-        this.pensao = dependente
-        this.dependente = salLiquido
+class Funcionario {
+    constructor (nome,salBruto,dependente,pensao,salLiquido) {
+        this.nomeClass = nome
+        this.salBrutoClass = salBruto
+        this.dependenteCLass = dependente
+        this.pensaoClass = pensao
+        this.salLiquidoClass = salLiquido
+        
+    }
+
+    static INSSdesconto(descontoINSS,/**/tetoINSS1,tetoINSS2,tetoINSS3,tetoINSS4,/**/taxaINSS1,taxaINSS2,taxaINSS3,taxaINSS4) {
+        this.descontoINSS = descontoINSS
+        
+        if(this.salBruto <= tetoINSS1) {
+            descontoINSS = this.salBruto * taxaINSS1
+        } else 
+        if (this.salBruto > tetoINSS1 && this.salBruto <= tetoINSS2) {
+            descontoINSS = this.salBruto * taxaINSS2
+        } else
+        if(this.salBruto > tetoINSS2 && this.salBruto <= tetoINSS3) {
+            descontoINSS = this.salBruto * taxaINSS3
+        } else 
+        if(this.salBruto > tetoINSS3 && this.salBruto <= tetoINSS4) {
+            descontoINSS = this.salBruto * taxaINSS4
+        } else
+        if(this.salBruto > tetoINSS4) {
+            descontoINSS = tetoINSS4 * taxaINSS4
+        }
+        return descontoINSS
+    }
+
+    static get calcular() {
+        let INSSdescontoVariavel = this.INSSdesconto(1212.01,2427.35,3641.03,7087.22,/**/0.075,0.09,0.12,0.14)
+        return INSSdescontoVariavel
     }
 }
