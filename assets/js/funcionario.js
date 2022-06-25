93bf6da07ebf191d51717d30b3f2243a269759bc
Gustavo Alfredo <gustavoalfredo.web@gmail.com>
2022-06-24 21:25:20 -0300

    teste

---
+++ assets/js/funcionario.js
@@ -0,0 +1,9 @@
+class funcionario {
+    constructor (nome,salBruto,pensao,dependente,salLiquido) {
+        this.nome = nome
+        this.salBruto = salBruto
+        this.pensao = pensao
+        this.pensao = dependente
+        this.dependente = salLiquido
+    }
+}
