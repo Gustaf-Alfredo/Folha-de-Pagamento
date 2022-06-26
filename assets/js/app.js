3c50223c0c07b8e1f809d532ca769ce9f73cbfe3
Gustavo Alfredo <gustavoalfredo.web@gmail.com>
2022-06-25 23:30:37 -0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ class App {
         let funcionario = new Funcionario(nome,inSalBruto,dependente,pensao)
         console.log(funcionario)
         this.salBruto = inSalBruto
-
+        //check
         if(dependente) {
             window.alert('check - dependente')
         }
@@ -17,14 +17,16 @@ class App {
             window.alert('check - pensao')
         }  
         
-        //let INSSdescontoVariavel = this.INSSdesconto(1212.01,2427.35,3641.03,7087.22,/**/0.075,0.09,0.12,0.14)
-        let INSSdescontoVariavel = this.INSSdesconto(this.descontoINSS,1212.01,2427.35,3641.03,7087.22,/**/0.075,0.09,0.12,0.14)
-        console.log(INSSdescontoVariavel)
+        //argumentos - INSSdesconto/IRPFdesconto
+        this.INSSdescontoVariavel = this.INSSdesconto(this.descontoINSS,1212.01,2427.35,3641.03,7087.22,/**/0.075,0.09,0.12,0.14)
+        this.IRPFdescontoVariavel = this.IRPFdesconto(this.descontoIRPF,1903.98,2826.66,3751.06,4664.68/**/,0.075,0.15,0.225,0.275,/**/142.8,354.8,636.13,869.36)
     }
 
+    //método responsável pelo cálculo INSS
     INSSdesconto(descontoINSS,/**/tetoINSS1,tetoINSS2,tetoINSS3,tetoINSS4,/**/taxaINSS1,taxaINSS2,taxaINSS3,taxaINSS4) {
         
         this.descontoINSS = descontoINSS
+
         if(this.salBruto <= tetoINSS1) {
             descontoINSS = this.salBruto * taxaINSS1
             console.log(this.salBruto)
@@ -45,10 +47,28 @@ class App {
         return descontoINSS
     }
 
-    calcular() {
-        let INSSdescontoVariavel = this.INSSdesconto(1212.01,2427.35,3641.03,7087.22,/**/0.075,0.09,0.12,0.14)
-        console.log(INSSdescontoVariavel)
-        return INSSdescontoVariavel
+    //método responsável pelo cálculo IRPF
+    IRPFdesconto(descontoIRPF,/**/tetoIRPF1,tetoIRPF2,tetoIRPF3,tetoIRPF4,/**/taxaIRPF1,taxaIRPF2,taxaIRPF3,taxaIRPF4,/**/deducoesIRPF1,deducoesIRPF2,deducoesIRPF3,deducoesIRPF4) {
+
+        this.baseCalculo = (this.salBruto) - (this.INSSdescontoVariavel)
+        this.descontoIRPF = descontoIRPF
+
+        if(this.baseCalculo <= tetoIRPF1) {
+            descontoIRPF = 0
+        } else 
+        if(this.baseCalculo > tetoIRPF1 && this.baseCalculo <= tetoIRPF2) {
+            descontoIRPF = (this.baseCalculo * taxaIRPF1) - deducoesIRPF1
+        } else
+        if(this.baseCalculo > tetoIRPF2 && this.baseCalculo <= tetoIRPF3) {
+            descontoIRPF = (this.baseCalculo * taxaIRPF2) - deducoesIRPF2
+        } else
+        if(this.baseCalculo > tetoIRPF3 && this.baseCalculo <= tetoIRPF4) {
+            descontoIRPF = (this.baseCalculo * taxaIRPF3) - deducoesIRPF3
+        } else
+        if(this.baseCalculo > tetoIRPF4) {
+            descontoIRPF = (this.baseCalculo * taxaIRPF4) - deducoesIRPF4
+        }
+        return descontoIRPF
         
     }
 }
